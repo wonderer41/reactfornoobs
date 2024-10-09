23a7be318b0b2f4d64be2ae1dceb18e3ad42f8bb
Gerardo Lazo <gerardoo.41@gmail.com>
2024-10-08 22:23:06 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Message from "./messege"
+import ListGroup from "./components/ListGroup";
+
+function App() {
+ /*  // const name = 'shuba';
+  if(name){
+  return <div>Hello {name}</div>
+  
+   }else{
+     //this is in case the const is empty
+     return <div>Hello {Message()}</div>
+   } */
+  return <div><ListGroup/></div>
+}
+export default App;
---
+++ src/messege.jsx
@@ -0,0 +1,7 @@
+
+
+//PascalCasing
+function Message(){
+    return <h4>Hello World</h4>
+}
+export default Message;
